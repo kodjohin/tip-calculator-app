970e69b40d451c9900cc38ce3027406b51885045
kodjohin <kodjo.ohin@gmail.com>
2021-12-03 22:48:13 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import { useState } from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 import Person from "./images/icon-person.svg";
 import Dollar from "./images/icon-dollar.svg";
+import CustomRadio from "./components/CustomRadio";
 
 const StyledApp = styled.div`
 	min-width: 375px;
@@ -14,50 +16,57 @@ const StyledApp = styled.div`
 const StyledMain = styled.main`
 	min-width: 375px;
 	display: flex;
-  flex-direction: column;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-  row-gap: 20px;
-  background-color: var(--white);
-  padding-top: 20px;
-  padding-bottom: 30px;
-  margin-top: 30px;
-  border-radius: 20px;
+	row-gap: 20px;
+	background-color: var(--white);
+	padding-top: 20px;
+	padding-bottom: 30px;
+	margin-top: 30px;
+	border-radius: 20px;
 
-  input {
-      font-size: 1.5rem;
-      border-radius: 3px;
-      padding: 5px 0;
-      text-align: right;
-      color: var(--very-dark-cyan);
-      font-weight: 700;
-      font-family: 'Space Mono', monospace;
-      padding-right: 10px;
+	input {
+		font-size: 1.5rem;
+		border-radius: 3px;
+		padding: 5px 0;
+		text-align: right;
+		color: var(--very-dark-cyan);
+		font-weight: 700;
+		font-family: "Space Mono", monospace;
+		padding-right: 10px;
+		outline: var(--strong-cyan);
 
-      &.bill {
-        background: url(${Dollar}) no-repeat scroll 7px 14px var(--very-light-grayish-cyan);
-        margin-bottom: 20px;
-      }
+		:focus {
+			border: 1px solid var(--strong-cyan);
+		}
 
-      &.people-number {
-        background: url(${Person}) no-repeat scroll 7px 14px var(--very-light-grayish-cyan);
-      }
+		&.bill {
+			background: url(${Dollar}) no-repeat scroll 7px 14px
+				var(--very-light-grayish-cyan);
+			margin-bottom: 20px;
+		}
 
-      /* Chrome, Safari, Edge, Opera */
-      ::-webkit-outer-spin-button,
-      ::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-      }
+		&.people-number {
+			background: url(${Person}) no-repeat scroll 7px 14px
+				var(--very-light-grayish-cyan);
+		}
 
-      /* Firefox */
-      [type=number] {
-        -moz-appearance: textfield;
-      }
-  }
-  section label {
-    margin: 10px 0;
-  }
+		/* Chrome, Safari, Edge, Opera */
+		::-webkit-outer-spin-button,
+		::-webkit-inner-spin-button {
+			-webkit-appearance: none;
+			margin: 0;
+		}
+
+		/* Firefox */
+		[type="number"] {
+			-moz-appearance: textfield;
+		}
+	}
+	section > label {
+		margin: 10px 0;
+	}
 
 	section {
 		display: flex;
@@ -67,102 +76,133 @@ const StyledMain = styled.main`
 
 const StyledButtons = styled.div`
 	display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 15px;
-  margin-bottom: 20px;
-
-  button {
-    padding: 10px 40px;
-    border-radius: 5px;
-    font-size: 1.2rem;
-    font-weight: 700;
-    font-family: 'Space Mono', monospace;
-    cursor: pointer;
-
-    &[value="5%"],
-    &[value="10%"],
-    &[value="25%"],
-    &[value="50%"]
-    {
-        background-color: var(--very-dark-cyan);
-        color: var(--white);
-    }
-    &[value="15%"]
-    {
-        background-color: var(--strong-cyan);
-        color: var(--very-dark-cyan);
-    }
-    &[value="Custom"]
-    {
-        background-color: var(--very-light-grayish-cyan);
-        color: var(--very-dark-grayish-cyan);
-    }
-  }
+	grid-template-columns: 1fr 1fr;
+	gap: 15px;
+	margin-bottom: 20px;
 `;
 
 const StyledSection = styled.div`
-  width: 90%;
+	width: 90%;
 	display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 20px 0px;
-  border-radius: 10px;
-  background-color: var(--very-dark-cyan);
-  text-align: center;
-  padding-top: 30px;
-  padding-bottom: 10px;
-  
-  button {
-    grid-column: span 2;
-    padding: 10px 85px;
-    border-radius: 5px;
-    margin: 10px 20px;
-    color: var(--very-dark-cyan);
-    background-color: var(--strong-cyan);
-    font-weight: 700;
-    font-family: 'Space Mono', monospace;
-  }
+	grid-template-columns: 1fr 1fr;
+	gap: 20px 0px;
+	border-radius: 10px;
+	background-color: var(--very-dark-cyan);
+	text-align: center;
+	padding-top: 30px;
+	padding-bottom: 10px;
 
-  label {
-      color: #fff;
-      text-align: left;
-      margin-left: 20px;
-      &.amount {
-        color: var(--strong-cyan);
-        font-size: 1.5rem;
-        text-align: right;
-        margin-right: 20px;
-      }
-      p {
-        font-size: .8rem;
-        color: var(--strong-cyan);
-      }
-  }
+	button {
+		grid-column: span 2;
+		padding: 10px 85px;
+		border-radius: 5px;
+		margin: 10px 20px;
+		color: var(--very-dark-cyan);
+		background-color: var(--strong-cyan);
+		font-weight: 700;
+		font-family: "Space Mono", monospace;
+    cursor: pointer;
+	}
+
+	label {
+		color: #fff;
+		text-align: left;
+		margin-left: 20px;
+		&.amount {
+			color: var(--strong-cyan);
+			font-size: 1.5rem;
+			text-align: right;
+			margin-right: 20px;
+		}
+		p {
+			font-size: 0.8rem;
+			color: var(--strong-cyan);
+		}
+	}
 `;
 
-function App() {
+const App = () => {
+	const [bill, setBill] = useState("");
+	const [people, setPeople] = useState("");
+	const [tip, setTip] = useState("5%");
+	const [tipAmount, setTipAmout] = useState(0);
+	const [total, setTotal] = useState(0);
+
+	const onBillChanged = (e) => {
+    console.log(e.target.value);
+    const value = e.target.value;
+    if(parseInt(value) === 0) return;
+    setBill(value);
+  };
+
+  const onTipChanged = (e) => {
+    console.log(e.target.value);
+    const value = e.target.value;
+    setTip(value);
+  };
+  const onCustomTipChanged = (e) => {
+    console.log(e.target.value);
+    const value = e.target.value;
+    setTip(value);
+  };
+
+  const onPeopleChanged = (e) => {
+    console.log(e.target.value);
+    const value = e.target.value;
+    if(parseInt(value) === 0) return;
+    setPeople(value);
+  };
+
+  const reset = () => {
+    console.log("%c Call Reset", "font-size: 20px; color: red;");
+    setTip("5%");
+    setBill("");
+    setPeople("");
+    setTipAmout(0);
+    setTotal(0);
+  }
+
 	return (
 		<StyledApp className="container">
 			<Header />
 			<StyledMain>
 				<section>
 					<label type="text">Bill</label>
-					<input className="bill"  type="number" name="" id=""/>
+					<input
+						className="bill"
+						type="number"
+						value={bill}
+						onChange={(e) => onBillChanged(e)}
+					/>
 					<label type="text">Select Tip %</label>
 					<StyledButtons>
-						{["5%", "10%", "15%", "25%", "50%", "Custom"].map((tip, index) => (
-							<button key={index} value={tip}>{tip}</button>
+						{["5%", "10%", "15%", "25%", "50%", "custom"].map((tip, index) => (
+							<CustomRadio id={`tip${index}`} name="tip" label={tip} key={index}  onChange={(e) => onTipChanged(e)}/>
 						))}
 					</StyledButtons>
-          <label type="text">Number of People</label>
-					<input className="people-number"  type="number" name="" id="" />
+					<label type="text">Number of People</label>
+					<input
+						className="people-number"
+						type="number"
+						value={people}
+						onChange={(e) => onPeopleChanged(e)}
+					/>
 				</section>
 				<StyledSection>
-          <label type="text">Tip Amount<p> / person</p></label>
-          <label className="amount" type="text">$4.27</label>
-          <label type="text">Total<p> / person</p></label>
-          <label className="amount" type="text">$32.79</label>
-          <button>RESET</button>
-        </StyledSection>
+					<label type="text">
+						Tip Amount<p> / person</p>
+					</label>
+					<label className="amount" type="text">
+						${tipAmount}
+					</label>
+					<label type="text">
+						Total<p> / person</p>
+					</label>
+					<label className="amount" type="text">
+						${total}
+					</label>
+					<button onClick={() => reset()}>RESET</button>
+				</StyledSection>
 			</StyledMain>
 		</StyledApp>
 	);
---
+++ src/components/CustomRadio.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledRadio = styled.div`
+	input {
+		display: none;
+	}
+	input {
+		text-align: center;
+		background-color: var(--very-light-grayish-cyan);
+		color: var(--very-dark-grayish-cyan);
+
+		::placeholder {
+			color: var(--very-dark-grayish-cyan);
+		}
+	}
+
+	input.custom {
+		width: 155px;
+		display: block;
+		text-align: center;
+	}
+	label.custom {
+		display: none;
+	}
+
+	input.custom {
+		:focus {
+			text-align: right;
+		}
+	}
+
+	label {
+		display: block;
+		font-family: "Space Mono", monospace;
+		cursor: pointer;
+		text-align: center;
+		border-radius: 5px;
+		font-size: 1.2rem;
+		font-weight: 700;
+		background-color: var(--very-dark-cyan);
+		color: var(--white);
+		padding: 10px 10px;
+	}
+
+	label:before {
+		transition: box-shadow 0.4s ease, background 0.3s ease;
+	}
+
+	input:checked + label {
+		background-color: var(--strong-cyan);
+		color: var(--very-dark-cyan);
+	}
+`;
+
+const CustomRadio = (props) => {
+	return (
+		<StyledRadio onChange={(e) => props.onChange(e)}>
+			{props.label === "custom" ? (
+				<>
+					<input
+						type="radio"
+						id={props.id}
+						value={props.label}
+						name={props.name}
+						defaultChecked={props.label === "custom"}
+					/>
+					<label className="custom" htmlFor={props.id}></label>
+					<input
+						className="custom"
+						type="number"
+						value=""
+						placeholder="Custom"
+						onChange={(e) => props.onChange(e)}
+					/>
+				</>
+			) : (
+				<>
+					<input
+						type="radio"
+						id={props.id}
+						value={props.label}
+						name={props.name}
+						defaultChecked={props.label === "5%"}
+					/>
+					<label htmlFor={props.id}>{props.label}</label>
+				</>
+			)}
+		</StyledRadio>
+	);
+};
+
+export default CustomRadio;
